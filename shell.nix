0b0eb5a139745bac5e3466352c653dd935d0e3b3
Emory Merryman <emory.merryman@gmail.com>
2021-03-06 15:09:42 -0500

    fix ubuntu

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
-{ pkgs ? import <nixpkgs> { } } : let
-	configuration = import ./configuration.nix ( structures : [
-
-	] ) ;
-in pkgs.mkShell {
+{ pkgs ? import <nixpkgs> { } } : pkgs.mkShell {
 	buildInputs = [
 		pkgs.vscode
-
-		pkgs.moreutils
+		(
+			pkgs.writeShellScriptBin "configure" ''
+				${ pkgs.gnupg }/bin/gpg --batch --import ./.private/gpg-private-keys.asc &&
+				${ pkgs.gnupg }/bin/gpg --import-ownertrust ./.private/gpg-ownertrust.asc &&
+				${ pkgs.gnupg }/bin/gpg2 --import ./.private/gpg2-private-keys.asc &&
+				${ pkgs.gnupg }/bin/gpg2 --import-ownertrust ./.private/gpg2-ownertrust.asc
+			''
+		)
 	] ;
 }
--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,9 @@
 
 /usr/bin/sudo /usr/bin/apt-get update --assume-yes &&
 /usr/bin/sudo /usr/bin/apt-get install --assume-yes curl &&
-/usr/bin/curl -L https://nixos.org/nix/install | /usr/bin/sudo /usr/bin/sh &&
+/usr/bin/sudo /usr/bin/mkdir /nix &&
+/usr/bin/sudo /usr/bin/chown $( /usr/bin/whoami ):$( /usr/bin/whoami ) /nix &&
+/usr/bin/curl -L https://nixos.org/nix/install | /usr/bin/sh &&
 /usr/bin/sudo /usr/bin/apt-get install --assume-yes direnv &&
 (
     /usr/bin/cat >> ${HOME}/.bashrc <<EOF
