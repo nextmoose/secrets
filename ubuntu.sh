f24e8a8f4b8c5d703474a8cd31b0d4da051c6afd
Emory Merryman <emory.merryman@gmail.com>
2021-03-06 13:01:43 -0500

    install rpi-imager

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,10 @@
 /usr/bin/sudo /usr/bin/apt-get install --assume-yes curl &&
 /usr/bin/curl -L https://nixos.org/nix/install | /usr/bin/sudo /usr/bin/sh &&
 /usr/bin/sudo /usr/bin/apt-get install --assume-yes direnv &&
-/usr/bin/cat >> ${HOME}/.bashrc <<EOF
+(
+    /usr/bin/cat >> ${HOME}/.bashrc <<EOF
 source ${HOME}.nix-profile/etc/profile.d/nix.sh
 eval "\$( /usr/bin/direnv hook bash )"
 EOF
+) &&
+/usr/bin/sudo snap install rpi-imager
