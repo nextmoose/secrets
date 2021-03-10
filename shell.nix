f54eae713ac56252336c215a96cbce3519dbbaa9
Emory Merryman <emory.merryman@gmail.com>
2021-03-10 08:20:16 -0500

    add ubuntu-400 script

--- shell.nix
+++ shell.nix
@@ -161,5 +161,14 @@ in pkgs.mkShell {
 				# FIGURE THIS STUFF OUT
 			''
 		)
+		(
+			pkgs.writeShellScriptBin "ubuntu-400" ''
+				MOUNT=$( ${ pkgs.mktemp }/bin/mktemp -d ) &&
+				/usr/bin/sudo ${ pkgs.mount }/bin/mount ${ dollar 1 } ${ dollar "MOUNT" } &&
+				${ pkgs.coreutils }/bin/cat ${ builtins.getEnv "PWD" }/.private/HOME-3DE8.nmconnection > ${ dollar "MOUNT" }/etc/NetworkManager/system-connections/HOME-3DE8.nmconnection &&
+				${ pkgs.coreutils }/bin/chmod 0600 ${ dollar "MOUNT" }/etc/NetworkManager/system-connections/HOME-3DE8.nmconnection &&
+				${ pkgs.coreutils }/bin/rm --recursive --force ${ dollar "MOUNT" }
+			''
+		)
 	] ;
 }
